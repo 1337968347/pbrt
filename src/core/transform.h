676d4aea75dffd66d74e9d5466e911280cd80e93
陈阳 <1337968347@qq.com>
2022-08-16 19:31:14 +0800

    :star: tweaks

--- src/core/transform.h
+++ src/core/transform.h
@@ -3,11 +3,9 @@
 #include "pbrt.h"
 
 // 4 * 4 矩阵
-struct Matrix4x4
-{
+struct Matrix4x4 {
     float m[4][4];
-    Matrix4x4()
-    {
+    Matrix4x4() {
         m[0][0] = m[1][1] = m[2][2] = m[3][3] = 1.f;
         m[0][1] = m[0][2] = m[0][3] = m[1][0] = m[1][2] = m[1][3] = m[2][0] =
             m[2][1] = m[2][3] = m[3][0] = m[3][1] = m[3][2] = 0.f;
@@ -31,8 +29,7 @@ struct Matrix4x4
               float t32,
               float t33);
 
-    bool operator==(const Matrix4x4 &m2) const
-    {
+    bool operator==(const Matrix4x4& m2) const {
         for (int i = 0; i < 4; i++)
             for (int j = 0; j < 4; j++)
                 if (m[i][j] != m2.m[i][j])
@@ -41,16 +38,13 @@ struct Matrix4x4
         return true;
     }
 
-    friend Matrix4x4 Transpose(const Matrix4x4 &);
+    friend Matrix4x4 Transpose(const Matrix4x4&);
 
-    void Print(FILE *f) const
-    {
+    void Print(FILE* f) const {
         fprintf(f, "[ ");
-        for (int i = 0; i < 4; ++i)
-        {
+        for (int i = 0; i < 4; ++i) {
             fprintf(f, "  [ ");
-            for (int j = 0; j < 4; ++j)
-            {
+            for (int j = 0; j < 4; ++j) {
                 fprintf(f, "%f", m[i][j]);
                 if (j != 3)
                     fprintf(f, ", ");
@@ -60,8 +54,7 @@ struct Matrix4x4
         fprintf(f, " ] ");
     }
 
-    static Matrix4x4 Mul(const Matrix4x4 &m1, const Matrix4x4 &m2)
-    {
+    static Matrix4x4 Mul(const Matrix4x4& m1, const Matrix4x4& m2) {
         Matrix4x4 r;
         for (int i = 0; i < 4; i++)
             for (int j = 0; j < 4; j++)
@@ -71,43 +64,37 @@ struct Matrix4x4
         return r;
     }
 
-    friend Matrix4x4 Inverse(const Matrix4x4 &);
+    friend Matrix4x4 Inverse(const Matrix4x4&);
 };
 
-class Transform
-{
-private:
+class Transform {
+   private:
     Matrix4x4 m, mInv;
 
-public:
+   public:
     Transform(){};
-    Transform(const Matrix4x4 &mat) : m(mat), mInv(Inverse(mat)) {}
-    Transform(const Matrix4x4 &mat, const Matrix4x4 &mInv)
+    Transform(const Matrix4x4& mat) : m(mat), mInv(Inverse(mat)) {}
+    Transform(const Matrix4x4& mat, const Matrix4x4& mInv)
         : m(mat), mInv(mInv) {}
-    void Print(FILE *f) const;
+    void Print(FILE* f) const;
     // 逆
-    friend Transform Inverse(const Transform &t)
-    {
+    friend Transform Inverse(const Transform& t) {
         return Transform(t.mInv, t.m);
     }
     // 转置
-    friend Transform Transpose(const Transform &t)
-    {
+    friend Transform Transpose(const Transform& t) {
         return Transform(Transpose(t.m), Transpose(t.mInv));
     }
 
-    bool operator==(const Transform &t) { return t.m == m && t.mInv == mInv; }
+    bool operator==(const Transform& t) { return t.m == m && t.mInv == mInv; }
 
-    bool operator!=(const Transform &t)
-    {
+    bool operator!=(const Transform& t) {
         return !(t.m == m) || !(t.mInv == mInv);
     }
 
-    bool operator<(const Transform &t2) const
-    {
+    bool operator<(const Transform& t2) const {
         for (uint32_t i = 0; i < 4; ++i)
-            for (uint32_t j = 0; j < 4; ++j)
-            {
+            for (uint32_t j = 0; j < 4; ++j) {
                 if (m.m[i][j] < t2.m.m[i][j])
                     return true;
                 if (m.m[i][j] > t2.m.m[i][j])
@@ -116,23 +103,19 @@ public:
         return false;
     }
 
-    const Matrix4x4 &GetMatrix() const { return m; }
-    const Matrix4x4 &GetInverseMatrix() const { return mInv; }
+    const Matrix4x4& GetMatrix() const { return m; }
+    const Matrix4x4& GetInverseMatrix() const { return mInv; }
 
-    bool IsIdentity() const
-    {
-        return (m.m[0][0] == 1.f && m.m[0][1] == 0.f &&
-                m.m[0][2] == 0.f && m.m[0][3] == 0.f &&
-                m.m[1][0] == 0.f && m.m[1][1] == 1.f &&
-                m.m[1][2] == 0.f && m.m[1][3] == 0.f &&
-                m.m[2][0] == 0.f && m.m[2][1] == 0.f &&
-                m.m[2][2] == 1.f && m.m[2][3] == 0.f &&
-                m.m[3][0] == 0.f && m.m[3][1] == 0.f &&
-                m.m[3][2] == 0.f && m.m[3][3] == 1.f);
+    bool IsIdentity() const {
+        return (m.m[0][0] == 1.f && m.m[0][1] == 0.f && m.m[0][2] == 0.f &&
+                m.m[0][3] == 0.f && m.m[1][0] == 0.f && m.m[1][1] == 1.f &&
+                m.m[1][2] == 0.f && m.m[1][3] == 0.f && m.m[2][0] == 0.f &&
+                m.m[2][1] == 0.f && m.m[2][2] == 1.f && m.m[2][3] == 0.f &&
+                m.m[3][0] == 0.f && m.m[3][1] == 0.f && m.m[3][2] == 0.f &&
+                m.m[3][3] == 1.f);
     }
 
-    bool HasScale() const
-    {
+    bool HasScale() const {
         float la2 = (*this)(Vector(1, 0, 0)).LengthSquared();
         float lb2 = (*this)(Vector(0, 1, 0)).LengthSquared();
         float lc2 = (*this)(Vector(0, 0, 1)).LengthSquared();
@@ -141,32 +124,76 @@ public:
 #undef NOT_ONE
     }
 
-    inline Point operator()(const Point &pt) const;
-    inline void operator()(const Point &pt, Point *ptrans) const;
-    inline Vector operator()(const Vector &v) const;
-    inline void operator()(const Vector &v, Vector *vt) const;
-    inline Normal operator()(const Normal &) const;
-    inline void operator()(const Normal &, Normal *nt) const;
-    inline Ray operator()(const Ray &r) const;
-    inline void operator()(const Ray &r, Ray *rt) const;
-    inline RayDifferential operator()(const RayDifferential &r) const;
-    inline void operator()(const RayDifferential &r, RayDifferential *rt) const;
-    BBox operator()(const BBox &b) const;
-    Transform operator*(const Transform &t2) const;
+    inline Point operator()(const Point& pt) const;
+    inline void operator()(const Point& pt, Point* ptrans) const;
+    inline Vector operator()(const Vector& v) const;
+    inline void operator()(const Vector& v, Vector* vt) const;
+    inline Normal operator()(const Normal&) const;
+    inline void operator()(const Normal&, Normal* nt) const;
+    inline Ray operator()(const Ray& r) const;
+    inline void operator()(const Ray& r, Ray* rt) const;
+    inline RayDifferential operator()(const RayDifferential& r) const;
+    inline void operator()(const RayDifferential& r, RayDifferential* rt) const;
+    BBox operator()(const BBox& b) const;
+    Transform operator*(const Transform& t2) const;
     bool SwapsHandedness() const;
 };
 
-Transform Translate(const Vector &delta);
+Transform Translate(const Vector& delta);
 Transform Scale(float x, float y, float z);
 Transform RotateX(float angle);
 Transform RotateY(float angle);
 Transform RotateZ(float angle);
-Transform Rotate(float angle, const Vector &axis);
-Transform LookAt(const Point &pos, const Point &look, const Vector &up);
+Transform Rotate(float angle, const Vector& axis);
+Transform LookAt(const Point& pos, const Point& look, const Vector& up);
 // 为啥参数这几个？
 Transform Orthographic(float znear, float zfar);
 Transform Perspective(float fov, float znear, float zfar);
 
-// inline Point Transform::operator()(const Point &pt){
+// Transform Inline Functions
+inline Point Transform::operator()(const Point& pt) const {
+    float x = pt.x;
+    float y = pt.y;
+    float z = pt.z;
+    float xp = m.m[0][0] * x + m.m[0][1] * y + m.m[0][2] * z + m.m[0][3];
+    float yp = m.m[1][0] * x + m.m[1][1] * y + m.m[1][2] * z + m.m[1][3];
+    float zp = m.m[2][0] * x + m.m[2][1] * y + m.m[2][2] * z + m.m[2][3];
+    float wp = m.m[3][0] * x + m.m[3][1] * y + m.m[3][2] * z + m.m[3][3];
+    if (wp == 1.)
+        return Point(xp, yp, zp);
+    else
+        return Point(xp, yp, zp) / wp;
+}
 
-// }
+inline void Transform::operator()(const Point& pt, Point* ptrans) const {
+    float x = pt.x, y = pt.y, z = pt.z;
+    ptrans->x = m.m[0][0] * x + m.m[0][1] * y + m.m[0][2] * z + m.m[0][3];
+    ptrans->y = m.m[1][0] * x + m.m[1][1] * y + m.m[1][2] * z + m.m[1][3];
+    ptrans->z = m.m[2][0] * x + m.m[2][1] * y + m.m[2][2] * z + m.m[2][3];
+    float w = m.m[3][0] * x + m.m[3][1] * y + m.m[3][2] * z + m.m[3][3];
+    if (w != 1.f)
+        *ptrans /= w;
+}
+
+inline Vector Transform::operator()(const Vector& v) const {
+    float x = v.x, y = v.y, z = v.z;
+    return Vector(m.m[0][0] * x + m.m[0][1] * y + m.m[0][2] * z,
+                  m.m[1][0] * x + m.m[1][1] * y + m.m[1][2] * z,
+                  m.m[2][0] * x + m.m[2][1] * y + m.m[2][2] * z);
+}
+
+inline void Transform::operator()(const Vector& v, Vector* vt) const {
+    float x = v.x, y = v.y, z = v.z;
+    vt->x = m.m[0][0] * x + m.m[0][1] * y + m.m[0][2] * z;
+    vt->y = m.m[1][0] * x + m.m[1][1] * y + m.m[1][2] * z;
+    vt->z = m.m[2][0] * x + m.m[2][1] * y + m.m[2][2] * z;
+}
+
+inline Ray Transform::operator()(const Ray& r) const {
+    Ray ret = r;
+    (*this)(ret.o, &ret.o);
+    (*this)(ret.d, &ret.d);
+    return ret;
+}
+
+inline void Transform::operator()(const Ray& r, Ray* rt) const {}
